0c46adfc9f5271314423e6a5713fd84d9cd53969
muyong <yong@max2.com>
2018-04-15 13:25:11 -0400

    Initial commit

---
+++ public/javascripts/callApp.js
@@ -0,0 +1,38 @@
+$("#callApp").click(function(){
+  //alert("will call app");
+  window.location.href = "callapp://startapp";
+});
+
+$(document).ready(function() {
+	ws = new WebSocket("ws://10.0.14.100:8025/ws/chat");
+
+	ws.onmessage = function(event) {
+	    var message = JSON.parse(event.data);
+		$("#messages").append("<p>" + message.content + "</p>");
+	};
+
+	ws.onclose = function() {
+		console.log("Socket closed");
+	};
+
+	ws.onopen = function() {
+		console.log("Connected");
+		var message = {
+		    "content": "Hello",
+		    "sender": navigator.userAgent,
+		    "received": ""
+		}
+		ws.send(JSON.stringify(message));
+	};
+
+	$("#new-message").bind("submit", function(event) {
+		event.preventDefault();
+        var message = {
+            "content": $("#message-text").val(),
+            "sender": navigator.userAgent,
+            "received": ""
+        }
+		ws.send(JSON.stringify(message));
+		$("#message-text").val("");
+	});
+});
